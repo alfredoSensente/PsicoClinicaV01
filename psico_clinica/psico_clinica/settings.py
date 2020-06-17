e5c4e6258454b8ee0f1edb858f5226fc6ed9a6d9
alfredo <ajsnsn93@gmail.com>
2020-06-17 14:56:31 -0600

    Me conecte a la psicoclinicadb en MariaDB

--- psico_clinica/psico_clinica/settings.py
+++ psico_clinica/psico_clinica/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'psico_clinica.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'psicoclinicadb',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
