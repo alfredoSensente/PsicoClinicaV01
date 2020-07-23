acd2469539b7a557b27b5522ff822997c5f76632
alfredo <ajsnsn93@gmail.com>
2020-07-22 21:32:11 -0600

    agregue el modelo de localidad a la app registro_paciente

--- psico_clinica/registro_paciente/models.py
+++ psico_clinica/registro_paciente/models.py
@@ -1,3 +1,19 @@
+"""modelos"""
 from django.db import models
 
 # Create your models here.
+class Localidad(models.Model):
+    """Localidad"""
+    id_localidad = models.AutoField(primary_key=True)
+    municipio = models.CharField(max_length=45)
+    departamento = models.CharField(max_length=45)
+    direccion = models.CharField(max_length=150)
+    telefono_fijo = models.CharField(max_length=45, blank=True, null=True)
+
+    def __str__(self):
+        """muestra el nombre del municipio"""
+        return self.telefono_fijo
+
+    class Meta:
+        managed = False
+        db_table = 'localidad'
