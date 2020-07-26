97dc5b85b44f27fad3d620a5ffa05b9a0aa65ab0
alfredo <ajsnsn93@gmail.com>
2020-07-25 22:30:25 -0600

    se agrego el modelo tipoReligion

--- psico_clinica/registro_paciente/models.py
+++ psico_clinica/registro_paciente/models.py
@@ -3,17 +3,28 @@ from django.db import models
 
 # Create your models here.
 class Localidad(models.Model):
-    """Localidad"""
+    """localidad"""
     id_localidad = models.AutoField(primary_key=True)
     municipio = models.CharField(max_length=45)
     departamento = models.CharField(max_length=45)
-    direccion = models.CharField(max_length=150)
+    direccion = models.CharField(max_length=255, blank=True, null=True)
     telefono_fijo = models.CharField(max_length=45, blank=True, null=True)
 
     def __str__(self):
-        """muestra el nombre del municipio"""
-        return self.telefono_fijo
+        return self.direccion
 
     class Meta:
         managed = False
         db_table = 'localidad'
+
+class TipoReligion(models.Model):
+    """tipo_religion"""
+    id_tipo_religion = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        managed = False
+        db_table = 'tipo_religion'
