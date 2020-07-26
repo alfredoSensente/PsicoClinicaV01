3a938a659c5e9288581c9797ad080f362e5bd7f6
alfredo <ajsnsn93@gmail.com>
2020-07-26 09:14:54 -0600

    se agregaron los modelos EstadoCivil, Referencia, Carrera, TipoFamiliar

--- psico_clinica/registro_paciente/models.py
+++ psico_clinica/registro_paciente/models.py
@@ -28,3 +28,51 @@ class TipoReligion(models.Model):
     class Meta:
         managed = False
         db_table = 'tipo_religion'
+
+class EstadoCivil(models.Model):
+    """estado civil"""
+    id_estado_civil = models.AutoField(primary_key=True)
+    estado_civil = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.estado_civil
+
+    class Meta:
+        managed = False
+        db_table = 'estado_civil'
+
+class Referencia(models.Model):
+    """referencia"""
+    id_referencia = models.AutoField(primary_key=True)
+    referencia = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.referencia
+
+    class Meta:
+        managed = False
+        db_table = 'referencia'
+
+class Carrera(models.Model):
+    """carrera"""
+    id_carrera = models.AutoField(primary_key=True)
+    carrera = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.carrera
+
+    class Meta:
+        managed = False
+        db_table = 'carrera'
+
+class TipoFamiliar(models.Model):
+    """tipo familiar"""
+    id_tipo_familiar = models.AutoField(primary_key=True)
+    nombre_tipo = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.nombre_tipo
+
+    class Meta:
+        managed = False
+        db_table = 'tipo_familiar'
