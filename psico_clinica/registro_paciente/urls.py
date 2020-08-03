8f8966beca77c7d379132f40418ea8b5d5a3e4b5
alfredo <ajsnsn93@gmail.com>
2020-08-03 11:26:37 -0600

    agrego app_name a urls de registro_paciente

--- psico_clinica/registro_paciente/urls.py
+++ psico_clinica/registro_paciente/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 from . import views
 
+app_name='registro_paciente'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:id_paciente>/', views.detalle_paciente, name='detalle_paciente'),
--- psico_clinica/registro_paciente/views.py
+++ psico_clinica/registro_paciente/views.py
@@ -1,6 +1,6 @@
 """vistas"""
 from django.shortcuts import render, get_object_or_404
-from .models import Paciente, Familiar
+from .models import Paciente
 
 # Create your views here.
 def index(request):
@@ -13,7 +13,7 @@ def index(request):
 
 def detalle_paciente(request, id_paciente):
     """detale_carrera"""
-    paciente_seleccionado = Paciente.objects.get(id_paciente=id_paciente)
+    paciente_seleccionado = get_object_or_404(Paciente, id_paciente=id_paciente)
     context = {
         'paciente_seleccionado' : paciente_seleccionado,
     }
